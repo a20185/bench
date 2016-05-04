1e500e2f50824ca409ae10a258fb4e89a90506e2
Souler Ou <ou@souler.me>
2016-05-04 11:21:42 +0800

    s

--- unixbench.sh
+++ unixbench.sh
@@ -11,14 +11,12 @@ wget http://www.ctohome.com/linux-vps-pack/unixbench-5.1.2.tar.gz;
 tar xzf unixbench-5.1.2.tar.gz;
 cd unixbench-5.1.2;
 
-yum -y install gcc gcc-c autoconf gcc-c++ time
+apt-get install -y install gcc gcc-c autoconf gcc-c++ time
 
 #Run unixbench
 sed -i "s/GRAPHIC_TESTS = defined/#GRAPHIC_TESTS = defined/g" ./Makefile
 make;
 ./Run;
-
-
 
 echo '';
 echo '';
